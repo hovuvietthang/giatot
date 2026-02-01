f730d50cd6ea9892a40bb06a0eeaf4b6361f0925
Hồ Vũ Việt Thắng <61175640+hovuvietthang@users.noreply.github.com>
2026-02-01 17:44:29 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Đã đổi sang BrowserRouter
 import { Layout } from './components/Layout';
 import { HomePage } from './pages/HomePage';
 import { RedirectPage } from './pages/RedirectPage';
 
-/**
- * Main Application Component
- * Using HashRouter for maximum compatibility in all environments.
- */
 const App: React.FC = () => {
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          {/* Hứng trực tiếp slug từ domain.com/slug */}
           <Route path="/:slug" element={<RedirectPage />} />
         </Routes>
       </Layout>
